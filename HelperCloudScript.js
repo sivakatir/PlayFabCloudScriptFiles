022e4e8dc2314831a09dfb8f5afa6f9b16e241b7
Siva Katir <sivakatir@users.noreply.github.com>
2015-12-14 22:30:47 -0800

    Update

--- HelperCloudScript.js
+++ HelperCloudScript.js
@@ -1,7 +1,7 @@
 var helpers = {
     helloWorld: function () {
         var result = {};
-        result.message = 'hello world 11';
+        result.message = 'hello world 12';
         return result;
     }
 };
